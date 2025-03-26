d903ef7e097885b1801381d249877d93ee2c4031
Ben Allred <skuttleman@gmail.coml>
2025-03-26 14:30:13 -0600

    run orphaned artifact cleanup continuously

--- src/brainard/core.clj
+++ src/brainard/core.clj
@@ -3,10 +3,10 @@
   (:gen-class)
   (:require
     [brainard.api.storage.interfaces :as istorage]
+    [brainard.api.utils.logger :as log]
     [brainard.api.utils.uuids :as uuids]
     [brainard.infra.utils.edn :as edn]
     [clojure.java.io :as io]
-    [clojure.pprint :as pp]
     [duct.core :as duct]
     [duct.core.env :as duct.env]
     [integrant.core :as ig]
@@ -29,16 +29,16 @@
                                   :args     [s3-keys]
                                   :xform    (map first)
                                   :history? true})]
-      (when-let [orphaned-ids (seq (remove (set db-keys) s3-keys))]
-        (println (format "cleaning up %s orphaned artifacts ..." (count orphaned-ids)))
-        (istorage/read obj-store
-                       {:op      :DeleteObjects
-                        :request {:Delete {:Objects (map #(hash-map :Key (str %))
-                                                         orphaned-ids)}}})
-        (println "... cleaned")))
+      (if-let [orphaned-ids (seq (remove (set db-keys) s3-keys))]
+        (do (log/info (format "cleaning up %s orphaned artifacts ..." (count orphaned-ids)))
+            (istorage/read obj-store
+                           {:op      :DeleteObjects
+                            :request {:Delete {:Objects (map #(hash-map :Key (str %))
+                                                             orphaned-ids)}}})
+            (log/info "... cleaned"))
+        (log/debug "no orphaned artifacts to clean")))
     (catch Throwable ex
-      (println "Failed to cleanup orphaned s3 objects")
-      (pp/pprint ex))))
+      (log/error ex "Failed to cleanup orphaned s3 objects"))))
 
 (defn start!
   "Starts a duct component system from a configuration expressed in an `edn` file."
@@ -51,10 +51,12 @@
                   (duct/prep-config profiles)
                   (ig/init [:duct/daemon]))
           thread (doto (Thread. (fn []
-                                  (letfn [(->comp [k]
-                                            (val (ig/find-derived-1 sys k)))]
-                                    (cleanup-orphaned-artifacts! (->comp :brainard/storage)
-                                                                 (->comp :brainard/obj-storage)))))
+                                  (let [db (val (ig/find-derived-1 sys :brainard/storage))
+                                        obj (val (ig/find-derived-1 sys :brainard/obj-storage))]
+                                    (while true
+                                      (cleanup-orphaned-artifacts! db obj)
+                                      ;; every 12 hours
+                                      (Thread/sleep ^long (* 1000 60 60 12))))))
                    .run)]
       (duct/add-shutdown-hook ::stop-thread #(.interrupt thread))
       sys)))
