63bc5d326dfc4910bbbf627f59240f20af72a29a
Ben Allred <skuttleman@gmail.coml>
2026-05-23 17:50:58 -0600

    refactor navigation test into one deftest

--- test/src/brainard/test/ui/navigation_test.clj
+++ test/src/brainard/test/ui/navigation_test.clj
@@ -4,35 +4,38 @@
     [clojure.test :refer [deftest is testing]]
     [etaoin.api :as eta]))
 
-(deftest home-page-test
+(deftest navigation-test
   (ui-sys/with-system [driver base-url]
-    (eta/go driver base-url)
-    (testing "renders app title"
-      (eta/wait-visible driver {:css "h1.title"})
-      (is (= "brainard" (eta/get-element-text driver {:css "h1.title"}))))
+    (testing "when visiting the home page"
+      (eta/go driver base-url)
+      (testing "renders app title"
+        (eta/wait-visible driver {:css "h1.title"})
+        (is (= "brainard" (eta/get-element-text driver {:css "h1.title"}))))
 
-    (testing "renders pinned notes section"
-      (is (true? (eta/wait-visible driver {:xpath "//h1/strong[text()='Pinned notes']"}))))
+      (testing "renders pinned notes section"
+        (is (true? (eta/wait-visible driver {:xpath "//h1/strong[text()='Pinned notes']"}))))
 
-    (testing "renders workspace section"
-      (is (true? (eta/wait-visible driver {:xpath "//h1/strong[text()='Workspace']"}))))))
+      (testing "renders workspace section"
+        (is (true? (eta/wait-visible driver {:xpath "//h1/strong[text()='Workspace']"}))))
 
-(deftest search-page-test
-  (ui-sys/with-system [driver base-url]
-    (eta/go driver (str base-url "/search"))
-    (testing "renders search form"
-      (is (true? (eta/wait-visible driver {:xpath "//button[text()='Search']"}))))
+      (testing "home nav item is active"
+        (let [el (eta/query driver {:css "li.is-active > a"})]
+          (is (= "Home" (eta/get-element-inner-html-el driver el))))))
 
-    (testing "search nav item is active"
-      (let [el (eta/query driver {:css "li.is-active > a"})]
-        (is (= "Search" (eta/get-element-inner-html-el driver el)))))))
+    (testing "when visiting the search page"
+      (eta/go driver (str base-url "/search"))
+      (testing "renders search form"
+        (is (true? (eta/wait-visible driver {:xpath "//button[text()='Search']"}))))
 
-(deftest buzz-page-test
-  (ui-sys/with-system [driver base-url]
-    (eta/go driver (str base-url "/buzz"))
-    (testing "renders app title"
-      (is (true? (eta/wait-visible driver {:css "h1.title"}))))
+      (testing "search nav item is active"
+        (let [el (eta/query driver {:css "li.is-active > a"})]
+          (is (= "Search" (eta/get-element-inner-html-el driver el))))))
 
-    (testing "buzz nav item is active"
-      (let [el (eta/query driver {:css "li.is-active > a"})]
-        (is (= "Buzz" (eta/get-element-inner-html-el driver el)))))))
+    (testing "when visiting the buzz page"
+      (eta/go driver (str base-url "/buzz"))
+      (testing "renders app title"
+        (is (true? (eta/wait-visible driver {:css "h1.title"}))))
+
+      (testing "buzz nav item is active"
+        (let [el (eta/query driver {:css "li.is-active > a"})]
+          (is (= "Buzz" (eta/get-element-inner-html-el driver el))))))))
