68bfef03290e47dafe0716e8061f1ff2789821ce
Ben Allred <skuttleman@gmail.coml>
2026-05-23 13:47:20 -0600

    setup CI tests

--- test/src/brainard/test/ui_system.clj
+++ test/src/brainard/test/ui_system.clj
@@ -6,8 +6,13 @@
 (defmacro with-system [[driver-binding base-url-binding] & body]
   `(tsys/with-system [{port# :cfg/server-port} {:config    "duct/ui-test.edn"
                                                 :init-keys [:brainard/webserver]}]
-     (let [~base-url-binding (str "http://localhost:" port#)
-           ~driver-binding (eta/chrome {:headless (= "true" (System/getenv "HEADLESS"))})]
+     (let [headless?# (= "true" (System/getenv "HEADLESS"))
+           ~base-url-binding (str "http://localhost:" port#)
+           ~driver-binding (eta/chrome {:headless    headless?#
+                                        :path-driver (or (System/getenv "CHROMEDRIVER_PATH") "chromedriver")
+                                        :args        (when headless?#
+                                                       ["--no-sandbox"
+                                                        "--disable-dev-shm-usage"])})]
        (try
          ~@body
          (finally
