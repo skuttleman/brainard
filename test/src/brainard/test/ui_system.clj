603da7be0c678dc6c9439fa9d20bf56e9e6bec8f
Ben Allred <skuttleman@gmail.coml>
2026-05-23 17:35:17 -0600

    add db seeding to ui tests

--- test/src/brainard/test/ui_system.clj
+++ test/src/brainard/test/ui_system.clj
@@ -1,19 +1,30 @@
 (ns brainard.test.ui-system
   (:require
+    [brainard.infra.db.store :as ds]
+    [brainard.infra.utils.edn :as edn]
     [brainard.test.system :as tsys]
+    [clojure.java.io :as io]
     [etaoin.api :as eta]))
 
-(defmacro with-system [[driver-binding base-url-binding] & body]
-  `(tsys/with-system [{port# :cfg/server-port} {:config    "duct/ui-test.edn"
-                                                :init-keys [:brainard/webserver]}]
-     (let [headless?# (= "true" (System/getenv "HEADLESS"))
-           ~base-url-binding (str "http://localhost:" port#)
-           ~driver-binding (eta/chrome {:headless    headless?#
-                                        :path-driver (or (System/getenv "CHROMEDRIVER_PATH") "chromedriver")
-                                        :args        (when headless?#
-                                                       ["--no-sandbox"
-                                                        "--disable-dev-shm-usage"])})]
-       (try
-         ~@body
-         (finally
-           (eta/quit ~driver-binding))))))
+(defmacro with-system [[driver-binding base-url-binding {:keys [seed]}] & body]
+  (let [seed (cond-> seed
+               (string? seed) (->> (str "fixtures/seed/")
+                                   io/resource
+                                   slurp
+                                   edn/read-string))]
+    `(tsys/with-system [{port# :cfg/server-port db# :brainard/IDBConn}
+                        {:config    "duct/ui-test.edn"
+                         :init-keys [:brainard/webserver]}]
+       (let [headless?# (= "true" (System/getenv "HEADLESS"))
+             ~base-url-binding (str "http://localhost:" port#)
+             ~driver-binding (eta/chrome {:headless    headless?#
+                                          :path-driver (or (System/getenv "CHROMEDRIVER_PATH")
+                                                           "chromedriver")
+                                          :args        (when headless?#
+                                                         ["--no-sandbox"
+                                                          "--disable-dev-shm-usage"])})]
+         (try
+           (ds/transact! db# ~seed)
+           ~@body
+           (finally
+             (eta/quit ~driver-binding)))))))
