f3aa99f2ff3b03fd7d38aaa8c913c1c84ea8895d
Ben Allred <skuttleman@gmail.coml>
2026-05-24 04:52:13 -0600

    workspace test

--- test/src/brainard/test/ui/core_test.clj
+++ test/src/brainard/test/ui/core_test.clj
@@ -1,4 +1,4 @@
-(ns brainard.test.ui.navigation-test
+(ns brainard.test.ui.core-test
   (:require
     [brainard.test.ui-system :as ui-sys]
     [clojure.test :refer [deftest is testing]]
@@ -39,3 +39,69 @@
       (testing "buzz nav item is active"
         (let [el (eta/query driver {:css "li.is-active > a"})]
           (is (= "Buzz" (eta/get-element-inner-html-el driver el))))))))
+
+(def ^:private ^:const node-item-selector-fmt
+  "//li[contains(@class,'node-item')]//span[text()='%s']")
+
+(deftest workspace-test
+  (ui-sys/with-system [driver base-url]
+    (letfn [(wait! [text]
+              (eta/wait-visible driver {:xpath (format node-item-selector-fmt text)}))
+            (edit! [node-text icon-class]
+              (let [edit-selector-fmt (str node-item-selector-fmt
+                                           "/following::i[contains(@class,'%s')][1]")
+                    xpath (format edit-selector-fmt node-text icon-class)]
+                (wait! node-text)
+                (eta/click-el driver (eta/query driver {:xpath xpath}))))
+            (submit! [content]
+              (ui-sys/submit-form! driver
+                                   ".modal-container.is-active form.form"
+                                   {"Content" content}))
+            (nodes [node-text]
+              (eta/query-all driver {:xpath (format node-item-selector-fmt node-text)}))]
+      (testing "when visiting the home page"
+        (eta/go driver base-url)
+
+        (testing "renders the empty workspace"
+          (eta/wait-visible driver {:xpath "//h1/strong[text()='Workspace']"})
+          (is (empty? (eta/query-all driver {:css "li.node-item"}))))
+
+        (testing "and when creating a workspace root node"
+          (eta/click driver {:css "div.drag-n-drop + button"})
+          (submit! "root node")
+
+          (testing "renders the updated workspace"
+            (wait! "root node")
+            (is (= 1 (count (eta/query-all driver {:css "li.node-item"}))))
+
+            (testing "and when creating a child node"
+              (edit! "root node" "lni-plus")
+              (submit! "child node")
+
+              (testing "renders the updated workspace"
+                (wait! "child node")
+                (is (= 2 (count (eta/query-all driver {:css "li.node-item"}))))
+
+                (testing "and when creating a sibling node"
+                  (edit! "root node" "lni-plus")
+                  (submit! "sibling node")
+
+                  (testing "renders the updated workspace"
+                    (wait! "sibling node")
+                    (is (= 3 (count (eta/query-all driver {:css "li.node-item"}))))))
+
+                (testing "and when creating a grandchild node"
+                  (edit! "child node" "lni-plus")
+                  (submit! "grandchild node")
+
+                  (testing "renders the updated workspace"
+                    (wait! "grandchild node")
+                    (is (= 4 (count (eta/query-all driver {:css "li.node-item"})))))))
+
+              (testing "and when updating the child node"
+                (edit! "child node" "lni-pencil")
+                (submit! "updated child")
+
+                (testing "renders the updated workspace"
+                  (wait! "updated child")
+                  (is (empty? (nodes "child node"))))))))))))
