a0b968d879e5695e60cb4309566ed041e16115e3
Ben Allred <skuttleman@gmail.coml>
2026-06-05 14:53:03 -0600

    improve ws reconnection logic

--- modules/infra/src/brainard/notifications/infra/manager.cljs
+++ modules/infra/src/brainard/notifications/infra/manager.cljs
@@ -1,5 +1,6 @@
 (ns brainard.notifications.infra.manager
   (:require
+    [brainard.api.utils.logger :as log]
     [brainard.infra.store.specs :as-alias specs]
     [brainard.infra.utils.edn :as edn]
     [clojure.core.async :as async]
@@ -8,15 +9,25 @@
     [haslett.client :as ws]))
 
 (defn ^:private ->conn []
-  (ws/connect "/api/ws"
-              {:in (async/chan 10 (map edn/read-string))}))
+  (log/info "establishing websocket connection...")
+  (ws/connect "/api/ws" {:in (async/chan 10 (map edn/read-string))}))
+
+(defn ^:private backoff [wait-ms]
+  (-> wait-ms
+      (* 7)
+      (max 1)
+      (min 3000)))
 
 (defn loop! [store]
-  (async/go-loop [stream (async/<! (->conn))]
-    (let [[_ {:keys [data]} :as val] (async/<! (:in stream))]
-      (when val
-        (defacto/emit! store [::res/swapped [::specs/notes#buzz] data]))
-      (if (and val (ws/connected? stream))
-        (recur stream)
-        (do (println "reconnecting...")
-            (recur (async/<! (->conn))))))))
+  (async/go-loop [stream (async/<! (->conn))
+                  new? true
+                  wait 0]
+    (when (and new? (ws/connected? stream))
+      (log/info "websocket connection established"))
+    (if (ws/connected? stream)
+      (let [[_ {:keys [data]} :as val] (async/<! (:in stream))]
+        (when val
+          (defacto/emit! store [::res/swapped [::specs/notes#buzz] data]))
+        (recur stream false 0))
+      (do (async/<! (async/timeout wait))
+          (recur (async/<! (->conn)) true (backoff wait))))))
